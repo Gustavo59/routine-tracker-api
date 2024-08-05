7a82ad42387c900510a3b8dd2a51af0adee50bf3
Gustavo Barros <gustavosbarros59@gmail.com>
2024-08-04 22:38:17 -0300

    fix imports

--- src/routine_tracker_core/tests/factories/__init__.py
+++ src/routine_tracker_core/tests/factories/__init__.py
@@ -1,4 +1,5 @@
 from .frequency_factory import FrequencyFactory
 from .historic_factory import HistoricFactory
 from .routine_factory import RoutineFactory
+from .tracker_factory import TrackerFactory
 from .user_factory import UserFactory
